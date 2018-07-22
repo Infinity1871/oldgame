d5b29e2780f4907b313f21146856c39d3cf2d4a1
e666666 <a0939712328@gmail.com>
2018-07-22 19:48:20 +0800

    Proof-on-concept for bug showing and formatting

--- javascripts/game.js
+++ javascripts/game.js
@@ -1,2 +1,36 @@
-idk = new Decimal(10)
-alert("Breakfinity.js working like a charm, js loads, everything good, now the game....")
+var player = {
+  bugs: new Decimal(10)
+}
+
+function format(num,decimalPoints=0,offset=0,rounded=true) {
+  num=new Decimal(num)
+  if (num.lt(10)&&!rounded) {
+	  var sub10=num.toFixed(decimalPoints)
+	  if (parseFloat(sub10)<10) return sub10
+  }
+  if (isNaN(num.mantissa)) {
+    return '?'
+  } else if (num.gte(1/0)) {
+    return 'Infinite'
+  } else if (num.lt(999.5)) {
+    return Math.round(num.toNumber())
+  } else {
+    var abbid=Math.max(Math.floor(num.e/3)-offset,0)
+    var mantissa=num.div(Decimal.pow(1000,abbid)).toFixed((abbid>0&&decimalPoints<2)?2:decimalPoints)
+    if (mantissa==Math.pow(1000,1+offset)) {
+      mantissa=mantissa/1000
+      abbid+=1
+    }
+    return (num.div(Decimal.pow(10,num.e)).toFixed((abbid>0&&decimalPoints<2)?2:decimalPoints))+"e"+num.e
+  }
+}
+
+
+function gameTick() {
+  player.bugs = player.bugs.add(1)
+  document.getElementById("bugs").innerHTML = format(player.bugs)
+}
+
+function gameStart() {
+  gameInterval = setInterval(gameTick,100)
+}
