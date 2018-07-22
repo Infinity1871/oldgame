d774bc72add53e97b2a4cd44b64a72acb9106ecf
e666666 <a0939712328@gmail.com>
2018-07-22 20:16:19 +0800

    It fucking works, holy ****

--- javascripts/game.js
+++ javascripts/game.js
@@ -1,5 +1,8 @@
 var player = {
-  bugs: new Decimal(10)
+  bugs: new Decimal(10),
+  DLAmount: [null,new Decimal(0),new Decimal(0),new Decimal(0),new Decimal(0)],
+  DLCost: [null,new Decimal(10),new Decimal(100),new Decimal(1e3),new Decimal(1e4)],
+  DLBPS: [null,new Decimal(1),new Decimal(10),new Decimal(50),new Decimal(50)]
 }
 
 function format(num,decimalPoints=0,offset=0,rounded=true) {
@@ -25,9 +28,20 @@ function format(num,decimalPoints=0,offset=0,rounded=true) {
   }
 }
 
+function buyDL(tier) {
+  if (player.DLCost[tier].gt(player.bugs)) return false;
+  player.bugs = player.bugs.sub(player.DLCost[tier])
+  player.DLAmount[tier] = player.DLAmount[tier].add(1)
+  return true
+}
 
 function gameTick() {
-  player.bugs = player.bugs.add(1)
+  for (i=1;i<2;i++) {
+    document.getElementById("dl" + i.toString() + "Amount").innerHTML = format(player.DLAmount[i])
+    document.getElementById("dl" + i.toString() + "BPS").innerHTML = format(player.DLAmount[i].times(player.DLBPS[i]))
+    document.getElementById("dl" + i.toString() + "Cost").innerHTML = format(player.DLCost[i])
+    player.bugs = player.bugs.add(player.DLAmount[i].times(player.DLBPS[i]).div(10))
+  }
   document.getElementById("bugs").innerHTML = format(player.bugs)
 }
 
