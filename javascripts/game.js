0bce6b71334ffff12823b26992d201e8a2bc18e7
e666666 <a0939712328@gmail.com>
2018-07-23 17:21:46 +0800

    Error effect v3

--- javascripts/game.js
+++ javascripts/game.js
@@ -87,14 +87,14 @@ function gameTick() {
   document.getElementById("CEReq").innerHTML = "10 " + MFNames[player.CE+4] + " Bought"
   document.getElementById("CEUnlocks").innerHTML = MFNames[player.CE+5]
   if (player.MFBought[8] >= 40 || true) {
-    target = getRandomInt(0,5)
-    target2 = getRandomInt(0,5)
+    target = getRandomInt(0,6)
+    target2 = getRandomInt(0,6)
     while (target2 == target) {
-      target2 = getRandomInt(0,5)
+      target2 = getRandomInt(0,6)
     }
     word = ["E","r","r","o","r"]
-    word[target] = String.fromCharCode(getRandomInt(33,126))
-    word[target2] = String.fromCharCode(getRandomInt(33,126))
+    word[target] = String.fromCharCode(getRandomInt(33,127))
+    word[target2] = String.fromCharCode(getRandomInt(33,127))
     document.getElementById("errorText").innerHTML = "500 Server " + word.join("")
   }
 }
